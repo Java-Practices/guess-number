9bfa89ab0a7bc4fda2e76b20dab3e41b7a3ad8b2
Eason Li <lijinghua@odc.cmbchina.cn>
2020-03-08 19:05:51 +0800

    Add app main method to start this game

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,40 @@
+import answer.InvalidAnswerException;
+import game.Game;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws InvalidAnswerException {
+        Scanner scanner = new Scanner(System.in);
+        Game game = new Game();
+        System.out.println("Guess numbers game starting, please enter four digits：");
+        do {
+            String guess = scanner.nextLine();
+            if (isCorrectInput(guess)) {
+                List<Integer> numbers = guess.chars().mapToObj(o -> Character.getNumericValue(o)).collect(Collectors.toList());
+                game.guess(numbers);
+                String result = game.getResult();
+                System.out.println(result);
+            }
+        } while (!game.isOver());
+    }
+    
+    private static boolean isCorrectInput(String input) {
+        long size = input.chars().distinct().count();
+        if (size != 4) {
+            System.out.println(input + " Wrong input");
+            return false;
+        }
+        
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException nfe) {
+            System.out.println(input + " Wrong input");
+            return false;
+        }
+        
+        return true;
     }
 }
